7ba999547779a7a358a45d0e348b18fb25cb94fb
ckblck <cookieblackdev@gmail.com>
2022-06-30 23:28:02 -0300

    Remove terser.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import image from "rollup-plugin-img";
 
@@ -75,8 +74,8 @@ export default {
         !production && livereload("public"),
 
         // If we're building for production (npm run build
-        // instead of npm run dev), minify
-        production && terser(),
+        // instead of npm run dev), minify // me -> terser() removed.
+        production,
     ],
     watch: {
         clearScreen: false,
